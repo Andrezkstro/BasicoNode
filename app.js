d26e6f4d442b9e9ce910168d6b4be06261aaf2b3
Andrez Kstro <andrezkstro@gmail.com>
2023-02-09 18:15:15 -0500

    App continuar curso

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ const http = require('http');
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    console.log(req.url, req.method, req.headers, req.body);
+    //process.exit(); salir de todo terminar el consumo 
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title> Hola </title> <head>');
+    res.write('<body><h1>Hola Hola </h1></body>');
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(port);
